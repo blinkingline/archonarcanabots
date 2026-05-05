773a67cd7e0d4654205d20436d3b1b1f408d6773
jim <james.jenista@gmail.com>
2026-05-04 19:08:07 -0700

    No need for a card type column in the illustrators CSV. Also added a small tool to merge a new batch of artists into the file.

--- models/artist_model.py
+++ models/artist_model.py
@@ -9,13 +9,12 @@ class ArtistMap(object):
             csv_reader = csv.reader(file)
             header = next(csv_reader)
             h0 = header[0] != 'card_title'
-            h1 = header[1] != 'card_type'
-            h2 = header[2] != 'illustrator_name'
-            if h0 or h1 or h2:
+            h1 = header[1] != 'illustrator_name'
+            if h0 or h1:
                 raise Exception('Check CSV schema!')
 
             for row in csv_reader:
-                self.m[row[0]] = row[2]
+                self.m[row[0]] = row[1]
 
     def get(self, card_title):
         if card_title not in self.m:
